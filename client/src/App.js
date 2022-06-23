9f2a0211187dde256c6727684477c8c57b16c4ae
Heneros <punk60826@gmail.com>
2022-06-23 03:09:08 +0300

    create and display users

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { useQuery } from '@apollo/client';
-import { GET_ALL_USERS } from './query/user';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ALL_USERS, GET_ONE_USER } from './query/user';
+import { CREATE_USER } from './mutations/user';
 
 const App = () => {
-    const { data, loading, error } = useQuery(GET_ALL_USERS);
-    const [users, setUsers] = useState([]);
-    console.log(data);
+    const { data, loading, error, refetch } = useQuery(GET_ALL_USERS, { pollInterval: 500 });
+
+
+    const {data:oneUser, loading: loadingOneUser} = useQuery(GET_ONE_USER, {
+        variables: {
+            id: 1
+        }
+    })
+    console.log(oneUser);
+    const [newUser] = useMutation(CREATE_USER)
+    const [users, setUsers] = useState([])
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState(0)
+
+
     useEffect(() => {
         if (!loading) {
-            setUsers(data.getAllUsers);
+            setUsers(data.getAllUsers)
         }
+    }, [data])
 
-    }, [data]);
+    const addUser = (e) => {
+        e.preventDefault()
+        newUser({
+            variables: {
+                input: {
+                    username, age
+                }
+            }
+        }).then(({ data }) => {
+            console.log(data)
+            setUsername('')
+            setAge(0)
+        })
+    }
+
+    const getAll = e => {
+        e.preventDefault()
+        refetch()
+    }
+    if (loading) {
+        return <h1>loading...</h1>
+    }
 
 
     return (
         <div>
             <form>
-                <input type="text" />
-                <input type="number" />
+                <input value={username} onChange={e => setUsername(e.target.value)} type="text" />
+                <input value={age} onChange={e => setAge(+e.target.value)} type="number" />
                 <div className="btns">
-                    <button>Create</button>
-                    <button>Get</button>
+                    <button onClick={(e) => addUser(e)}>Create</button>
+                    <button onClick={e => getAll(e)}>Get</button>
                 </div>
             </form>
             <div>
--- client/src/query/user.js
+++ client/src/query/user.js
@@ -1,10 +1,17 @@
-import { gql } from '@apollo/client';
-
+import { gql } from '@apollo/client'
 
 export const GET_ALL_USERS = gql`
-  query{
-    getAllUsers {
-        id, username
-    }
-  }
-`;
+    query {
+        getAllUsers {
+            id, username, age
+        }
+    }    
+`
+
+export const GET_ONE_USER = gql`
+    query getUser($id: ID){
+        getUser(id: $id) {
+            id, username
+        }
+    }    
+`
